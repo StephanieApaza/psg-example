30f8e1a60d9177c0ce840ef0288e9b9a319a8908
Stephanie Apaza <stef.53apza@gmail.com>
2025-05-06 09:36:06 -0400

    Tutorial de como lavar platos

---
+++ retos_sesion_03/ejercicio_03.py
@@ -0,0 +1,35 @@
+print("TUTORIAL: Como lavar los platos paso a paso")
+
+print("Paso 1: Preparar los materiales")
+print("- Esponja recomendada: usar una esponja de doble cara (una suave y otra rugosa).")
+print("- Jabón recomendado: utiliza jabón líquido para vajilla.")
+
+print("Paso 2: Organizar los platos sucios")
+print("- Retira los restos de comida de cada plato y tíralos a la basura.")
+print("- Separa los utensilios: vasos, platos, ollas, sartenes y cubiertos.")
+
+print("Paso 3: Preparar el área de lavado")
+print("- Llena una parte del lavaplatos con agua.")
+print("- Llena la otra parte con agua limpia para enjuagar.")
+
+print("Paso 4: Lavar los utensilios")
+print("- Agarra la esponja y untala con el jabon liquido y sumergela en el agua para comenzar a lavar.")
+print("- Comienza lavando los vasos, ya que son los menos sucios.")
+print("- Luego lava los cubiertos con cuidado, prestando atención a los cuchillos.")
+print("- Continúa con los platos, lavando ambos lados.")
+print("- Por último, lava las ollas y sartenes, usando el lado rugoso de la esponja para eliminar residuos pegados.")
+
+print("Paso 5: Enjuagar bien")
+print("- Enjuaga cada objeto con agua limpia para eliminar todo el jabón.")
+print("- Asegúrate de que no queden restos resbalosos de detergente.")
+
+print("Paso 6: Secar los platos")
+print("- Puedes usar un paño limpio y seco para secar a mano.")
+print("- O puedes colocar los platos en un escurridor para que se sequen al aire.")
+
+print("Paso 7: Limpiar y guardar")
+print("- Limpia la esponja con agua y déjala escurrir.")
+print("- Limpia el lavaplatos para evitar malos olores.")
+print("- Guarda los utensilios una vez estén completamente secos.")
+
+print("¡Tarea terminada! Tus platos están limpios y tu cocina ordenada.")

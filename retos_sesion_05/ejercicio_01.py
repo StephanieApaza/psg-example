7be4cc9d40957bbe30a17c725018e646f4f8d5df
Stephanie Apaza <stef.53apza@gmail.com>
2025-05-21 17:56:16 -0400

    Calculo de salario

---
+++ retos_sesion_05/ejercicio_01.py
@@ -0,0 +1,12 @@
+# Calculo de salario 
+# Cantidad de horas trabajadas es 160 y la tarifa por hoea es de 5.5 USD/hora
+
+# Definición de variables
+cantidad_horas = 160
+tarifa = 5.5
+
+# Calculo del salario
+salario = cantidad_horas * tarifa
+
+# Resultado
+print("El salario es:",salario,"USD")

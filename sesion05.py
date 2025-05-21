ff4f7c65d32dc3e709ed5f3e51a7ce5d473f7546
Stephanie Apaza <stef.53apza@gmail.com>
2025-05-21 17:45:51 -0400

    Tipos de datos numericos: Integer y floating

---
+++ sesion05.py
@@ -0,0 +1,116 @@
+# Tipo entero
+print(10)
+print(type(10))
+
+variable = 100
+print(variable)
+print(type(variable))
+
+# variable 20 entero
+variable_2 = int(20)
+print(variable_2)
+print(type(variable_2))
+
+# Valor 10 en base decimal
+print("Base decimal")
+print(10)
+# Valor 10 binario
+print("Base binaria")
+print(0b1010)
+# Valor 10 en octal
+print("Base octal")
+print(0o12)
+# Valor 10 en hexadecimal
+print("Base hexadecimal")
+print(0xa)
+
+# Declarar enteros grandes
+variable_3 = 123456789123456789123456789123456789123456789123456789123456789
+print(variable_3)
+print(type(variable_3))
+
+# Valor float
+print(0.5)
+print(type(0.5))
+
+# Variable 0.100546
+variable_4 = 0.100464
+print(variable_4)
+print(type(variable_4))
+
+# Variable 1 flotante
+variable_7 = float(1)
+print(variable_7)
+print(type(variable_7))
+
+# Precision de 17 decimales
+variable_5 = 0.99999999999999999
+print(variable_5)
+print(type(variable_5))
+
+# Precision de 17 decimales
+variable_5 = 0.99999999999999999
+print(variable_5 == 1)
+print(type(variable_5))
+
+# Valor 2.0e-3 Flotante
+variable_6 = 2.0e-3
+print(variable_6)
+print(type(variable_6))
+
+# Operadores aritmeticos
+a = 10
+b = 3
+print("Suma")
+print(a + b)
+
+# Resta
+print("Resta")
+print(a - b)
+
+# Multiplicacion
+print("Multiplicacion")
+print(a * b)
+
+# Division
+print("Division")
+print(a / b)
+
+# Potencia
+print("Potencia")
+print(a ** b)
+
+# Division entera
+print("Division entera")
+print(a // b)
+
+# Modulo o residuo
+print("Modulo o residuo")
+print(a % b)
+
+# Operaciones más complejas
+c = 300/60
+s = (3600/60)/60
+d = c + s
+print(d)
+
+# Operadores de comparación
+print("Operadores de comparación")
+comparar = 10
+print(comparar < 10)
+print(comparar > 10)
+print(comparar == 10)
+print(comparar <= 10)
+print(comparar >= 10)
+print(comparar != 10)
+
+# Operadores de comparación (valor)
+entero = 10
+flotante = 10.0
+print(entero < flotante)
+print(entero > flotante)
+print(entero == flotante)
+print(entero >= flotante)
+print(entero <= flotante)
+print(entero != flotante)
+print("34" == 34)

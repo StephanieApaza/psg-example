2bc300cba292f839a87be13e5ae5f5525fcca053
Stephanie Apaza <stef.53apza@gmail.com>
2025-05-22 10:01:39 -0400

    Cálculo del volumen de un cubo

---
+++ retos_sesion_05/ejercicio_04.py
@@ -0,0 +1,10 @@
+# Calcular el volumen de un cubo donde uno de sus lados mide 10 metros
+
+# Declaración de variables
+a = 10
+
+# Formula del volumen de un cubo
+volumen = a ** 3
+
+# Resultado
+print("El volumen del cubo es de: ", volumen, "metros")

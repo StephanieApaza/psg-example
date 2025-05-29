4b182c548cfb803cfe6943313a1bdb39aa96fecb
Stephanie Apaza <stef.53apza@gmail.com>
2025-05-29 09:59:15 -0400

    Determinando si el cuadrado y la suma de 2 y 2 son iguales

---
+++ retos_sesion_06/ejercicio_07.py
@@ -0,0 +1,7 @@
+# ¿El cuadrado y la suma de 2 y 2 son iguales?
+
+num = 2
+cuadrado_num = num ** 2
+suma = 2 + 2
+igualdad = cuadrado_num == suma
+print("El cuadrado y la suma de 2 y 2 son iguales?: ", igualdad)

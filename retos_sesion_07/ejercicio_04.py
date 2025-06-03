39533f772070cdf7357a737f7ef703b09632a32c
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-03 10:42:09 -0400

    Invirtiendo la palabra con slicing

---
+++ retos_sesion_07/ejercicio_04.py
@@ -0,0 +1,5 @@
+# Invierte la siguiente palabra "electroencefalografista"
+
+palabra = "electroencefalografista"
+palab_invert = palabra[::-1]
+print("La palabra invertida tiene como resutado: ", palab_invert)

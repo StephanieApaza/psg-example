3bbc15c8be17ea11f3973d31a4d999642ebbe1f5
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-02 20:29:44 -0400

    Programa que recibe tres enteros y resta su resultado

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,8 @@
+# Escribe un programa que reciba tres enteros por teclado y reste su resultado
+
+num1 = int(input("Escribe un número entero: "))
+num2 = int(input("Escribe un segundo número entero: "))
+num3 = int(input("Escribe un tercer número entero: "))
+
+resta = num1 - num2 - num3
+print("La resta de los tres números enteros es: ", resta) 

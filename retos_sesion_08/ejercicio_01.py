f2a32f3fcffa99a58a732d789941f02bfc5cbe95
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-08 16:40:52 -0400

    Programa que almacena tres coordenadas en un tupla

---
+++ retos_sesion_08/ejercicio_01.py
@@ -0,0 +1,10 @@
+# Ingresa por teclado tres coordenadas 'x', 'y' 'z', convierte los valores a enteros que almacenan los tres valores un una tupla
+
+print("Este programa almacena tres coordenadas en una tupla")
+
+x = int(input("Escribe un número entero para el valor de x: "))
+y = int(input("Escribe un número entero para el valor de y: "))
+z = int(input("Escribe un número entero para el valor de z: "))
+
+coordenadas = (x, y, z)
+print("Las coordenadas escritas son: ", coordenadas)

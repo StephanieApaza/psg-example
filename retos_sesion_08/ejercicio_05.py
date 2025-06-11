51dd7b34b3dcf5a6c4c2e6620844fd199d301b40
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-11 10:00:22 -0400

    Tupla anidada de una guía telefónica

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,13 @@
+# De la guía telefónica genera un tupla anidada e imprime el resultado
+
+# Detalle de la guía telefónica
+dato1 = ("Jhon", 591123456, "Villa Fatima")
+dato2 = ("Jane", 591546324, "Obrajes")
+dato3 = ("Dogg", 591222421, "Miraflores")
+dato4 = ("Kity", 591753159, "Achumani")
+
+# Tupla anidada
+guia = (dato1, dato2, dato3, dato4)
+
+# Resultados
+print(guia)

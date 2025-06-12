20de1e9d7c6b8612c30fe0b9d54cb99bac64e5cd
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-12 10:27:34 -0400

    Encontrando el punto medio de las coordenadas e imprimiendo el resultado en una tupla

---
+++ retos_sesion_08/ejercicio_06.py
@@ -0,0 +1,19 @@
+# De las tuplas de coordenadas (x1,y1) = (-20,-20) y (x2,y2) = (40,20) encuentra las coordenadas del punto medio, almacena en una
+# tupla e imprime el resultado (x,y)
+
+# Fórmula del punto medio: ((x1 + x2)/2, (y1 + y2)/2)
+
+# Coordenadas
+x1 = -20
+y1 = -20
+x2 = 40
+y2 = 20
+
+# Aplicación de la fórmula
+x = (x1 + x2)/2
+y = (y1 + y2)/2
+
+# Punto medio
+punto_medio = (x, y)
+
+print("El punto medio de las coordenadas es: ",punto_medio)

2b8cdb8a79c4d6299a3bfd9aecef40d1bc248f54
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-16 12:00:57 -0400

    Lista con números enteros y flotantes utilizando índices para ver sus elementos y tipo de datos

---
+++ retos_sesion_09/ejercicio_01.py
@@ -0,0 +1,18 @@
+# Crear una lista con 5 números flotantes y 5 enteros y mostrar sus elementos y tipos utilizando indices
+
+# Lista con números flotantes y enteros
+numeros = [3.5, 45, 3.2, 73, 9.1, 40, 15.4, 3, 26, 36.8]
+
+# Elementos y tipos
+print("Lista de números con sus elementos y tipos de datos:\n")
+
+print(f"{"Indice 0: "} {numeros[0]} {"-"} {type(numeros[0])}")
+print(f"{"Indice 1: "} {numeros[1]} {"-"} {type(numeros[1])}")
+print(f"{"Indice 2: "} {numeros[2]} {"-"} {type(numeros[2])}")
+print(f"{"Indice 3: "} {numeros[3]} {"-"} {type(numeros[3])}")
+print(f"{"Indice 4: "} {numeros[4]} {"-"} {type(numeros[4])}")
+print(f"{"Indice 5: "} {numeros[5]} {"-"} {type(numeros[5])}")
+print(f"{"Indice 6: "} {numeros[6]} {"-"} {type(numeros[6])}")
+print(f"{"Indice 7: "} {numeros[7]} {"-"} {type(numeros[7])}")
+print(f"{"Indice 8: "} {numeros[8]} {"-"} {type(numeros[8])}")
+print(f"{"Indice 9: "} {numeros[9]} {"-"} {type(numeros[9])}")

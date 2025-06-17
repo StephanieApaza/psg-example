106ea7d785843f8d4813991ec4bb487d92d470ea
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-17 10:04:30 -0400

    Lista inversa utilizando saltos de 3 en 3

---
+++ retos_sesion_09/ejercicio_02.py
@@ -0,0 +1,7 @@
+# De la lista obtener una sub lista inversa utilizando saltos de 3 en 3
+
+numeros = [5,4,3,2,2,2,0,0,1,2]
+print(numeros)
+
+sub_num = numeros[::-3]
+print("Sub lista inversa con saltos de 3 en 3: ", sub_num)

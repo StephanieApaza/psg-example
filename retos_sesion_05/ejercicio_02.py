0537f5ff5f8da4dccef288a6d02e132d4a651fbb
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-26 22:09:46 -0400

    Conversión de temperaturas de grados Fahrenheit a Celsius de acuerdo a las instrucciones

--- retos_sesion_05/ejercicio_02.py
+++ retos_sesion_05/ejercicio_02.py
@@ -1,16 +1,16 @@
-# Conversión de temperaturas de grados Celsius a Fahrenheit
+# Conversión de temperaturas de grados Fahrenheit a Celsius
 
-# Temperaturas en grados Celcius
-grado_1 = 30
-grado_2 = -273.99
-grado_3 = 100
+# Temperaturas en grados Fahrenheit
+grado_1 = 25
+grado_2 = 300
+grado_3 = 76
 
-# Conversiones
-a = (grado_1 * 9/5) + 32
-b = (grado_2 * 9/5) + 32
-c = (grado_3 * 9/5) + 32
+# Conversión a Celsius
+a = (grado_1 - 32) * 5/9
+b = (grado_2 - 32) * 5/9
+c = (grado_3 - 32) * 5/9
 
 # Resultados
-print(grado_1,"°C =",a,"°F")
-print(grado_2,"°C =",b,"°F")
-print(grado_3,"°C =",c,"°F")
+print(grado_1, "°F es igual a =", round(a,2), "°C")
+print(grado_2, "°F es igual a =", round(b,2), "°C")
+print(grado_3, "°F es igual a =", round(c,2), "°C")

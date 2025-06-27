3ce55204f3216a3bf7b0b66100513bb923bb15a4
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-27 00:09:03 -0400

    Construcción del operador NOR utilizando operadores lógicos

--- retos_sesion_06/ejercicio_02.py
+++ retos_sesion_06/ejercicio_02.py
@@ -1,7 +1,25 @@
 # Construyendo el operador XNOR
 
 print("Operador XNOR")
-print((15 > 7) == (55 > 15))
-print((15 <= 7) == (55 > 15))
-print((15 > 7) == (5 + 5 > 15))
-print((15 < 7) == (55 < 15))
+
+print("   A    |    B   |  A XNOR B")
+print("-----------------------------")
+a = True
+b = True
+c = (a and b) or (not a and not b)
+print(" ",a," | ",b," | ",c)
+
+a = True
+b = False
+c = (a and b) or (not a and not b)
+print(" ",a," | ",b,"| ",c)
+
+a = False
+b = True
+c = (a and b) or (not a and not b)
+print(" ",a,"|",b,"  | ",c)
+
+a = False
+b = False
+c = (a and b) or (not a and not b)
+print(" ",a,"| ",b,"| ",c)

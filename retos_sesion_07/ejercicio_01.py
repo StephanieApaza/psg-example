2d112564b2e53ede4fa611b883fd65945f2f1d86
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-27 18:01:49 -0400

    Implementé triple comillas para declarar texto largo

--- retos_sesion_07/ejercicio_01.py
+++ retos_sesion_07/ejercicio_01.py
@@ -1,4 +1,7 @@
 # Escribir una cadena con una frase de un libro o un poema
 
-frase = "Si tu actos inspiran a otros a soñar más, aprender más, hacer más y ser más, eres un líder."
+frase = """Si tu actos inspiran a otros a soñar más,
+aprender más,
+hacer más y ser más,
+eres un líder."""
 print(frase)

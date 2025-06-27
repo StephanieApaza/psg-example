aad77910e55c52a535b73ca0f20a077a67bf5083
Stephanie Apaza <stef.53apza@gmail.com>
2025-06-26 22:42:32 -0400

    Conversión de 1000000 segundos a semanas, días, horas, minutos y segundos de acuerdo a las instrucciones

--- retos_sesion_05/ejercicio_03.py
+++ retos_sesion_05/ejercicio_03.py
@@ -1,24 +1,34 @@
-# Conversión a días, horas, minutos y segundos
+# Conversión de 1000000 segundos a semanas, días, horas, minutos y segundos
 
 # Cantidad de segundos totales
-segundos = 288325
+t_segundos = 1000000
 
-# Calculo de segundos a días
-dias = segundos // 86400
-tiempo_restante = segundos % 86400
+# Cantidad de segundos por cada unidad de tiempo
+s_minuto = 60
+s_hora = 60 * 60
+s_dia = 24 * 60 * 60
+s_semana = 7 * 24 * 60 * 60
 
-# Calculo de segundos restantes a horas
-horas = tiempo_restante // 3600
-tiempo_restante = tiempo_restante % 3600
+# Calculo de cuantas semanas hay en 1000000 segundos
+semanas = t_segundos // s_semana
+resto = t_segundos % s_semana
 
-# Calculo de segundos restantes a minutos
-minutos = tiempo_restante // 60
+# Calculo de cuantos días hay en el resto de segundos
+dias = resto // s_dia
+resto = resto % s_dia
 
-# Calculo de segundos restantes finales
-segundos_rest = tiempo_restante % 60
+# Calculo de cuantas horas hay en el resto de segundos
+horas = resto // s_hora
+resto = resto % s_hora
+
+# Calculo de cuantos minutos hay en el resto de segundos
+minutos = resto // s_minuto
+segundos = resto % s_minuto
 
 # Resultado
-print("Dias: ", dias)
-print("Horas: ", horas)
-print("Minutos: ", minutos)
-print("Segundos: ", segundos_rest)
+print("1000000 segundos equivalen a:")
+print(semanas, "semanas")
+print(dias, "días")
+print(horas, "horas")
+print(minutos, "minutos")
+print(segundos, "segundos")

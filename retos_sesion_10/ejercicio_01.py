89d58e9dcea97098d54fb7973b64f879a6c57027
Stephanie Apaza <stef.53apza@gmail.com>
2025-07-01 18:57:59 -0400

    Eliminando elementos repetidos de la cadena utilizando set

---
+++ retos_sesion_10/ejercicio_01.py
@@ -0,0 +1,7 @@
+# Elimina los elementos de oficina repetidos de la cadena
+
+items = ('📎','📐','📏','✏️','🖊️','🖋️','📎','📌','📏','📇','🗂️','📁','📌','🗃️','✏️','📂','🖇️')
+
+# Declarando set debido a que no permite elementos repetidos
+conjunto_items = set(items)
+print("Los elementos de la oficina son:", conjunto_items)

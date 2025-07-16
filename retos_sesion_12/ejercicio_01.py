a82900e34aac75d1fb1510b08acd521ae566a87c
Stephanie Apaza <stef.53apza@gmail.com>
2025-07-15 21:03:19 -0400

    Verificando si es múltiplo de 5, utilizando if y operador ternario

---
+++ retos_sesion_12/ejercicio_01.py
@@ -0,0 +1,7 @@
+# Crear un script que pida un número entero y verifique si es múltiplo de 5 usando operador ternario
+
+print("Inicio si es múltiplo de 5")
+numero = int(input("Escribe un número entero: "))
+multiplo_cinco = "Es múltiplo de 5" if numero % 5 == 0 else "No es múltiplo de 5"
+print(multiplo_cinco)
+print("Fin")

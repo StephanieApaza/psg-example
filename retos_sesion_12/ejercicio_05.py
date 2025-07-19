4234d09a54b3adf518620fe5f74292cb4dd38bda
Stephanie Apaza <stef.53apza@gmail.com>
2025-07-18 22:23:34 -0400

    Modificación de la estructura lógica del inicio de la app para gestionar contactos

--- retos_sesion_12/ejercicio_05.py
+++ retos_sesion_12/ejercicio_05.py
@@ -5,10 +5,15 @@ contactos = {}
 nombre = input("Escribe el nombre de contacto: ").strip().title()
 numero_telefono = input("Escribe +(código del país) seguido del número de teléfono: ").strip()
 
-if nombre and numero_telefono.startswith('+') and numero_telefono[1:].isdigit() and len(numero_telefono) == 12:
+es_valido = nombre and numero_telefono.startswith('+') and numero_telefono[1:].isdigit() and len(numero_telefono[1:]) == 11
+
+print("Nombre: ", nombre)
+print("Teléfono: ", numero_telefono)
+print("-----------------------")
+
+if es_valido:
     contactos[nombre] = numero_telefono
     print("Contacto guardado")
-    print("Lista de contactos: ",contactos)
 else:
     print("Datos incorrectos")
 

ae162ef401c66075ac1c670cec88109875283f97
Stephanie Apaza <stef.53apza@gmail.com>
2025-08-13 09:03:29 -0400

    Aplicación de la definición de conjuntos a partir de una lista

--- retos_sesion_10/ejercicio_04.py
+++ retos_sesion_10/ejercicio_04.py
@@ -4,13 +4,16 @@
 postres_Jane = ['Lemon Pie', 'Brownie', 'Tarta de Manzana','Helado de Chocolate', 'Flan']
 postres_Jhon = ['Carrot Cake', 'Croissant de Chocolate', 'Lemon Pie', 'Tarta de Manzana', 'Pudding']
 
+set_postres_Jane = set(postres_Jane)
+set_postres_Jhon = set(postres_Jhon)
+
 # Cantidad total de postres que pidieron
-postres = set(postres_Jane).union(postres_Jhon)
+postres = set_postres_Jane.union(set_postres_Jhon)
 total_postres = len(postres)
 
 # Postres en común que comparten Jane y Jhon
-postres_comun = set(postres_Jane).intersection(postres_Jhon)
-print("Los postres en común que pidieron Jane y Jhon son: ",postres_comun)
+postres_comun = set_postres_Jane.intersection(set_postres_Jhon)
+print("Los postres en común que pidieron Jane y Jhon son: ", postres_comun)
 
 # Cantidad de postres que tienen en común
 cant_postres = len(postres_comun)

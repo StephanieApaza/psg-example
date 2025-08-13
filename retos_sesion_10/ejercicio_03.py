1f2fd2be2ecb6012ccb71b02c0f239252c439a8d
Stephanie Apaza <stef.53apza@gmail.com>
2025-08-13 08:55:52 -0400

    Aplicación de la definición de conjuntos a partir de una lista

--- retos_sesion_10/ejercicio_03.py
+++ retos_sesion_10/ejercicio_03.py
@@ -3,14 +3,17 @@
 tienda_fisica = ["Ana", "Luis", "Pedro", "María", "Juan"]
 tienda_online = ["Pedro", "María", "Ana", "Carlos", "Laura"]
 
+set_tienda_fisica = set(tienda_fisica)
+set_tienda_online = set(tienda_online)
+
 # Quiénes compraron en ambos canales
-ambas_tiendas = set(tienda_fisica).intersection(tienda_online)
+ambas_tiendas = set_tienda_fisica.intersection(set_tienda_online)
 print("Las personas que compraron en ambas tiendas son: ", ambas_tiendas)
 
 # Quiénes compraron solo en la tienda física
-only_fisica = set(tienda_fisica).difference(tienda_online)
+only_fisica = set_tienda_fisica.difference(set_tienda_online)
 print("Las personas que compraron solo en la tienda fisica son: ", only_fisica)
 
 # Quiénes compraron solo online
-only_online = set(tienda_online).difference(tienda_fisica)
+only_online = set_tienda_online.difference(set_tienda_fisica)
 print("Las personas que compraron solo en la tienda online son: ", only_online)

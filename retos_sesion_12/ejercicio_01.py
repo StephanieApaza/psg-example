cebc8bc67be0b67f72506fd37e1642b58af82d2d
Stephanie Apaza <stef.53apza@gmail.com>
2025-08-14 09:38:59 -0400

    Aplicando truthiness en las evaluaciones lógicas

--- retos_sesion_12/ejercicio_01.py
+++ retos_sesion_12/ejercicio_01.py
@@ -2,6 +2,6 @@
 
 print("Inicio si es múltiplo de 5")
 numero = int(input("Escribe un número entero: "))
-multiplo_cinco = "Es múltiplo de 5" if numero % 5 == 0 else "No es múltiplo de 5"
+multiplo_cinco = "Es múltiplo de 5" if not(numero % 5) else "No es múltiplo de 5"
 print(multiplo_cinco)
 print("Fin")
